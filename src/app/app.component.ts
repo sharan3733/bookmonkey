92fa698be02b2356a4f8f263ecf820715eab2b28
sharanjeetkaur <sharanjeet.kaur@lt.hs-fulda.de>
2023-04-14 08:58:56 +0200

    make component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import { Book } from './book';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookmonkey-client';
+  title = 'FirstBootCamp';
+ public books: Book[] = [{
+    title: 'Hello Angular',
+    author: 'sukhman',
+    abstract:'loremgmwrmgkreemthhnmlmhkiti'
+  },
+  {
+    title: 'Hello Java',
+    author: 'sukhu',
+    abstract:'loremgmwrmgkreemthhnmlmhkiti qsFAMFF NKJGN KGNG RK'
+  }
+]
+  handleEmitFromChild(evt:Book){
+    console.log(evt);
+  }
+
 }
---
+++ src/app/book-card/book-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Book } from '../book';
+
+@Component({
+  selector: 'app-book-card',
+  templateUrl: './book-card.component.html',
+  styleUrls: ['./book-card.component.scss']
+})
+export class BookCardComponent {
+
+  @Input() content!: Book;
+  @Output() detailClick = new EventEmitter<Book>();
+customstyle ={
+  color: 'green'
+};
+handleDetailedClicked():void{
+  this.detailClick.emit(this.content);
+}
+}
